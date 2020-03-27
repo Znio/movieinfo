e16cc42cdefee8f6c23d7ec6d7d4793e2b968aaf
Xylon Gracias <xylongracias@gmail.com>
2020-03-27 16:41:03 +0530

    Updates

---
+++ precache-manifest.4402352b7e57c6845d28782f8b8bda1a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4876125fa25054be8e5a8d527e7108d0",
+    "url": "/cineinfo/index.html"
+  },
+  {
+    "revision": "8849b2d94e608ecef58a",
+    "url": "/cineinfo/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "42fbc102d549ab5b7e08",
+    "url": "/cineinfo/static/css/main.964e8bac.chunk.css"
+  },
+  {
+    "revision": "8849b2d94e608ecef58a",
+    "url": "/cineinfo/static/js/2.6588eb17.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cineinfo/static/js/2.6588eb17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42fbc102d549ab5b7e08",
+    "url": "/cineinfo/static/js/main.61def860.chunk.js"
+  },
+  {
+    "revision": "5ecdc6960025959aa49e",
+    "url": "/cineinfo/static/js/runtime-main.3a9c0e08.js"
+  },
+  {
+    "revision": "a0de630520d964bc0f29cfc709135096",
+    "url": "/cineinfo/static/media/banner.a0de6305.jpg"
+  },
+  {
+    "revision": "f353939276fecf4ea393660c0149e767",
+    "url": "/cineinfo/static/media/default-image-portrait.f3539392.png"
+  },
+  {
+    "revision": "19650bb5104695b59b80924f7c3a51dd",
+    "url": "/cineinfo/static/media/icomoon.19650bb5.woff"
+  },
+  {
+    "revision": "305de33799d87002d139cf0ddd32db07",
+    "url": "/cineinfo/static/media/icomoon.305de337.svg"
+  },
+  {
+    "revision": "4fb33a21a97f63a09f544758da3dfc90",
+    "url": "/cineinfo/static/media/icomoon.4fb33a21.ttf"
+  },
+  {
+    "revision": "f449ca93664bb9c99c7658f71314a2d1",
+    "url": "/cineinfo/static/media/icomoon.f449ca93.eot"
+  },
+  {
+    "revision": "6a38fde958508418f917066783f7a9fe",
+    "url": "/cineinfo/static/media/logo.6a38fde9.svg"
+  }
+]);
